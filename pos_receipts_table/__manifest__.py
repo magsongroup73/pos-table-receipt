ff3e2dff01b57d5dd5701991b574fb566da00998
Magson <99962086+magsongroup73@users.noreply.github.com>
2026-09-06 18:10:03 +0500

    Update __manifest__.py

--- pos_receipts_table/__manifest__.py
+++ pos_receipts_table/__manifest__.py
@@ -2,6 +2,7 @@
     'name': 'POS VAT  Receipt',
     'version': '19.0.1.1',
     'category': 'Point of Sale',
+    'author': 'Hidext',
     'summary': ' POS receipt with VAT Invoice heading and VAT No',
     'description': """
     This module customizes the Point of Sale receipt:
